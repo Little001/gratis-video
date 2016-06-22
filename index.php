7017686b7f184fb6b9345f55be9cbb11ceffb420
mmeravy <mmeravy@localhost.localdomain>
2016-06-22 14:52:15 +0200

    news

--- index.php
+++ index.php
@@ -6,27 +6,29 @@
     
     
     
-    $servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "serials";
+    // $servername = "localhost";
+	// $username = "root";
+	// $password = "";
+	// $dbname = "serials";
 	
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
+	// // Create connection
+	// $conn = new mysqli($servername, $username, $password, $dbname);
 	
-	// Check connection
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-		//echo "<script>console.log('Connected to DB failed!');</script>";
-	} 
+	// // Check connection
+	// if ($conn->connect_error) {
+	//     die("Connection failed: " . $conn->connect_error);
+	// 	//echo "<script>console.log('Connected to DB failed!');</script>";
+	// } 
     
-    $ser = array();
-    // $sql = "SELECT * FROM serials ORDER BY visited DESC";
-	// $result = $conn->query($sql);  
-    // if ($result->num_rows > 0){
-    //     while($row = $result->fetch_assoc()) {
-    //         array_push($ser, $row["name"]);
-    //     }
-    // } 
-    echo $twig->render('index.html', ['serials' =>  $ser]);
+    // $ser = array();
+    // // $sql = "SELECT * FROM serials ORDER BY visited DESC";
+	// // $result = $conn->query($sql);  
+    // // if ($result->num_rows > 0){
+    // //     while($row = $result->fetch_assoc()) {
+    // //         array_push($ser, $row["name"]);
+    // //     }
+    // // } 
+    echo $twig->render('header.html');
+    echo $twig->render('index.html');
+    echo $twig->render('footer.html');
 ?>
---
+++ test.php
@@ -0,0 +1,21 @@
+<?php 
+	require_once __DIR__ . '/vendor/autoload.php';
+	$loader = new Twig_Loader_Filesystem('Resources/views');
+	$twig = new Twig_Environment($loader);
+	
+
+    
+    $ser = array();
+    // $sql = "SELECT * FROM serials ORDER BY visited DESC";
+	// $result = $conn->query($sql);  
+    // if ($result->num_rows > 0){
+    //     while($row = $result->fetch_assoc()) {
+    //         array_push($ser, $row["name"]);
+    //     }
+    // } 
+    
+    echo $twig->render('head.html');
+    echo $twig->render('body.html');
+    echo $twig->render('footer.html');
+    
+?>
